b15a54de86458b4101b73a2d94defb75d5ae14cc
Roman Chursanov <authfailed@gmail.com>
2024-10-07 18:27:57 +0500

    Заменен base в конфиге

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://authfailed.github.io",
-  base: "/flomaster",
+  site: "https://cm.chrsnv.ru",
+  base: "/",
 
   integrations: [
     starlight({
