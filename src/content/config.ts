a7402856190e3f94ba53a2d7e809210226e0b637
Roman Chursanov <authfailed@gmail.com>
2024-10-15 07:16:18 +0500

    Добавлен предупреждающий банер на все страницы
    Signed-off-by: Roman Chursanov <authfailed@gmail.com>

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,15 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema } from '@astrojs/starlight/schema';
+import {defineCollection, z} from 'astro:content'
+import {docsSchema, i18nSchema} from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+	docs: defineCollection({
+		schema: docsSchema({
+			extend: z.object({
+				banner: z.object({content: z.string()}).default({
+					content: 'Инструмент находится в <a href="https://github.com/AuthFailed/chat-flomaster">активной разработке</a>. Не использовать в работе!',
+				}),
+			}),
+		})
+	}),
+	i18n: defineCollection({type: 'data', schema: i18nSchema()}),
 };
