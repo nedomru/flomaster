82f94e3c3ff09afeab645a7eaba71a793077f482
Roman Chursanov <authfailed@gmail.com>
2024-11-09 08:36:52 +0500

    Удалено логирование envs
    Signed-off-by: Roman Chursanov <authfailed@gmail.com>

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -6,8 +6,6 @@ const supabaseRoleKey = import.meta.env.PUBLIC_SUPABASE_SERVICE_ROLE_KEY
 if (!supabaseUrl || !supabaseRoleKey) {
     console.error("[Supabase] There are some configuration missing");
     throw new Error(`Supabase configuration is missing, url: ${supabaseUrl}, key: ${supabaseRoleKey}`);
-}else {
-    console.info(`supabaseUrl: ${supabaseUrl}, key: ${supabaseRoleKey}`);
 }
 
 export const supabase = createClient(supabaseUrl, supabaseRoleKey, {
