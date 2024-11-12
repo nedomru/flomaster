fdfc41f58e3c4763b25cf5132bf13afd83ed2f4e
Roman Chursanov <authfailed@gmail.com>
2024-11-12 11:56:07 +0500

    Добавлены все категории РМов в CMS
    Signed-off-by: Roman Chursanov <authfailed@gmail.com>

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -8,7 +8,7 @@ export default config({
 
     collections: {
         'tech/internet': collection({
-            label: 'Технина - Интернет',
+            label: 'Технина/Интернет',
             slugField: 'title',
             path: 'src/content/docs/chat-phrases/tech/internet/**',
             entryLayout: 'content',
@@ -16,12 +16,12 @@ export default config({
             schema: {
                 title: fields.slug({ name: { label: 'Title' } }),
                 content: fields.mdx({
-                    label: 'Content',
+                    label: 'Контент',
                 }),
             },
         }),
         'tech/setup': collection({
-            label: 'Технина - Настройка оборудования',
+            label: 'Технина/Настройка оборудования',
             slugField: 'title',
             path: 'src/content/docs/chat-phrases/tech/setup/**',
             entryLayout: 'content',
@@ -29,12 +29,12 @@ export default config({
             schema: {
                 title: fields.slug({ name: { label: 'Title' } }),
                 content: fields.mdx({
-                    label: 'Content',
+                    label: 'Контент',
                 }),
             },
         }),
         'tech/television': collection({
-            label: 'Технина - Телевидение',
+            label: 'Технина/Телевидение',
             slugField: 'title',
             path: 'src/content/docs/chat-phrases/tech/television/**',
             entryLayout: 'content',
@@ -42,12 +42,12 @@ export default config({
             schema: {
                 title: fields.slug({ name: { label: 'Title' } }),
                 content: fields.mdx({
-                    label: 'Content',
+                    label: 'Контент',
                 }),
             },
         }),
         'tech/intercom': collection({
-            label: 'Технина - Домофония',
+            label: 'Технина/Домофония',
             slugField: 'title',
             path: 'src/content/docs/chat-phrases/tech/intercom/**',
             entryLayout: 'content',
@@ -55,12 +55,12 @@ export default config({
             schema: {
                 title: fields.slug({ name: { label: 'Title' } }),
                 content: fields.mdx({
-                    label: 'Content',
+                    label: 'Контент',
                 }),
             },
         }),
         'tech/cctv': collection({
-            label: 'Технина - Видеонаблюдение',
+            label: 'Технина/Видеонаблюдение',
             slugField: 'title',
             path: 'src/content/docs/chat-phrases/tech/cctv/**',
             entryLayout: 'content',
@@ -68,12 +68,12 @@ export default config({
             schema: {
                 title: fields.slug({ name: { label: 'Название' } }),
                 content: fields.mdx({
-                    label: 'Content',
+                    label: 'Контент',
                 }),
             },
         }),
-        'tech/incidents': collection({
-            label: 'Технина - Заявки и аварии',
+        'tech/accidents': collection({
+            label: 'Технина/Заявки и аварии',
             slugField: 'title',
             path: 'src/content/docs/chat-phrases/tech/accidents/**',
             entryLayout: 'content',
@@ -81,33 +81,87 @@ export default config({
             schema: {
                 title: fields.slug({ name: { label: 'Название' } }),
                 content: fields.mdx({
-                    label: 'Content',
+                    label: 'Контент',
                 }),
             },
         }),
-        dialog: collection({
-            label: 'Диалог',
+        'dialog/communications': collection({
+            label: 'Диалог/Общение с клиентом',
             slugField: 'title',
-            path: 'src/content/docs/chat-phrases/dialog/**',
+            path: 'src/content/docs/chat-phrases/dialog/communications/**',
+            entryLayout: 'content',
             format: { contentField: 'content' },
             schema: {
-                title: fields.slug({ name: { label: 'Title' } }),
+                title: fields.slug({ name: { label: 'Название' } }),
                 content: fields.mdx({
-                    label: 'Content',
+                    label: 'Контент',
                 }),
             },
-        },),
-        sales: collection({
-            label: 'Продажи',
+        }),
+        'dialog/companylaw': collection({
+            label: 'Диалог/Правовая часть',
             slugField: 'title',
-            path: 'src/content/docs/chat-phrases/sales/**',
+            path: 'src/content/docs/chat-phrases/dialog/companylaw/**',
+            entryLayout: 'content',
             format: { contentField: 'content' },
             schema: {
-                title: fields.slug({ name: { label: 'Title' } }),
+                title: fields.slug({ name: { label: 'Название' } }),
                 content: fields.mdx({
-                    label: 'Content',
+                    label: 'Контент',
                 }),
             },
-        },),
+        }),
+        'dialog/reglaments': collection({
+            label: 'Диалог/Регламенты',
+            slugField: 'title',
+            path: 'src/content/docs/chat-phrases/dialog/reglaments/**',
+            entryLayout: 'content',
+            format: { contentField: 'content' },
+            schema: {
+                title: fields.slug({ name: { label: 'Название' } }),
+                content: fields.mdx({
+                    label: 'Контент',
+                }),
+            },
+        }),
+        'sales/routers': collection({
+            label: 'Продажи/Роутеры',
+            slugField: 'title',
+            path: 'src/content/docs/chat-phrases/sales/routers/**',
+            entryLayout: 'content',
+            format: { contentField: 'content' },
+            schema: {
+                title: fields.slug({ name: { label: 'Название' } }),
+                content: fields.mdx({
+                    label: 'Контент',
+                }),
+            },
+        }),
+        'sales/decoders': collection({
+            label: 'Продажи/Декодеры',
+            slugField: 'title',
+            path: 'src/content/docs/chat-phrases/sales/decoders/**',
+            entryLayout: 'content',
+            format: { contentField: 'content' },
+            schema: {
+                title: fields.slug({ name: { label: 'Название' } }),
+                content: fields.mdx({
+                    label: 'Контент',
+                }),
+            },
+        }),
+        'sales/camcorders': collection({
+            label: 'Продажи/Камеры',
+            slugField: 'title',
+            path: 'src/content/docs/chat-phrases/sales/camcorders/**',
+            entryLayout: 'content',
+            format: { contentField: 'content' },
+            schema: {
+                title: fields.slug({ name: { label: 'Название' } }),
+                content: fields.mdx({
+                    label: 'Контент',
+                }),
+            },
+        }),
     },
 });
