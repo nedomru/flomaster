7d9c6f1e475a659d04492a6124cbbeb797294b3d
Roman Chursanov <roman@chrsnv.ru>
2025-12-26 19:40:51 +0500

    feat: Ссылка на чат

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,14 @@
-import {defineCollection} from 'astro:content'
+import {defineCollection, z} from 'astro:content'
 import {docsSchema, i18nSchema} from '@astrojs/starlight/schema';
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { docSearchI18nSchema } from '@astrojs/starlight-docsearch/schema';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	docs: defineCollection({ loader: docsLoader(), schema: docsSchema({extend: z.object({
+				banner: z.object({ content: z.string() }).default({
+					content: "Чатик только для классных спецов <a href='https://t.me/+2vVZ0vXJiWFkOWZi'>тута</a>",
+				}),
+			}),}
+		) }),
 	i18n: defineCollection({type: 'data', schema: i18nSchema({ extend: docSearchI18nSchema() })}),
 };
